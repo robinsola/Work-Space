4d1852f0bcfc1de719504d84f35dd5dd4880f305
Robin Sola <rsolarms@gmail.com>
2018-06-07 15:52:46 -0700

    add components with routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ContactComponent,
+    HomeComponent,
+    GalleryComponent,
+    CalendarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,28 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { LoginComponent } from './login/login.component';
+
+
+const appRoutes: Routes = [
+{ path: '',
+  component: HomeComponent
+},
+{ path: 'calendar',
+  component: CalendarComponent
+},
+{ path: 'contact',
+  component: ContactComponent
+},
+{ path: 'gallery',
+  component: GalleryComponent
+},
+{ path: 'login',
+  component: LoginComponent
+}
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
