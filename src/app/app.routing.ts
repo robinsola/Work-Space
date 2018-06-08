984a90dd5ed01ca9df1e96bdd5fb028939acc647
Robin Sola <rsolarms@gmail.com>
2018-06-08 08:56:11 -0700

    fix syntax errors

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,21 +8,21 @@ import { LoginComponent } from './login/login.component';
 
 
 const appRoutes: Routes = [
-{ path: '',
-  component: HomeComponent
-},
-{ path: 'calendar',
-  component: CalendarComponent
-},
-{ path: 'contact',
-  component: ContactComponent
-},
-{ path: 'gallery',
-  component: GalleryComponent
-},
-{ path: 'login',
-  component: LoginComponent
-}
+  { path: '',
+    component: HomeComponent
+  },
+  { path: 'calendar',
+    component: CalendarComponent
+  },
+  { path: 'contact',
+    component: ContactComponent
+  },
+  { path: 'gallery',
+    component: GalleryComponent
+  },
+  { path: 'login',
+    component: LoginComponent
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
